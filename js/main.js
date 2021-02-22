b0d3bc1d4a2282b44a3a3029db76129640d7d472
Edoardo <edoardo.malfatto@hotmail.it>
2021-02-22 18:56:00 +0100

    completato

--- js/main.js
+++ js/main.js
@@ -5,26 +5,64 @@ var km = document.getElementById("km");
 var eta = document.getElementById("eta");
 var prezzo
 var genera = document.getElementById("genera");
+var row2 = document.getElementById("row2");
+var annulla = document.getElementById("annulla");
 
 //costanti
 const PrezzoAlKm = 0.21;
 const ScontoMinorenni = 0.8;
 const ScontoAnziani = 0.4;
 
-//bottone "genera"
+//modifica input in active
+nome.addEventListener("click",
+    function () {
+        nome.className = "active_color";       
+    }
+);
+km.addEventListener("click",
+    function () {
+        km.className = "active_color";       
+    }
+);
+eta.addEventListener("click",
+    function () {
+        eta.className = "active_color";       
+    }
+);
+genera.addEventListener("click",
+    function () {
+        genera.className = "active_color";       
+    }
+);
 
-//copia nome in row2
+//bottone "annulla"
+annulla.addEventListener("click",
+    function () {
+        location.reload()
+   
+    }
+);
+
+//bottone "genera"
 genera.addEventListener("click",
     function () {
         console.log("inizio+");
+        row2.className = "display_block";
         document.getElementById('nome_row').innerHTML = (nome.value);
         if (eta.value == "Minorenne") {
             document.getElementById('offerta').innerHTML = ("Offerta Minorenne");
         } else if (eta.value == "Over65") {
-            document.getElementById('offerta').innerHTML = ("Offerta Over65");
+            document.getElementById('offerta').innerHTML = ("Offerta Silver");
         } else if (eta.value == "Maggiorenne") {
-            document.getElementById('offerta').innerHTML = ("Offerta standard");
+            document.getElementById('offerta').innerHTML = ("Offerta Standard");
         }
+        
+        
+        //numeri casuali di carrozza e codice
+        document.getElementById('carrozza').innerHTML =Math.floor(Math.random() * 15) + 1;
+        document.getElementById('codice').innerHTML =Math.floor(Math.random() * 10000) + 99999;;
+        
+
         //Calcolo prezzo per età
         if (eta.value == "Minorenne") {
             prezzo = (km.value * PrezzoAlKm * ScontoMinorenni);
